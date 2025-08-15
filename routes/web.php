042d20b3ad3750e29a667b7556595f924458da2f
DiegoLeandro477 <diegoleandro477@gmail.com>
2025-08-15 11:38:30 -0300

    create pages and controllers of curses

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\CursoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class,'index'])->name('users.index');
 ;
 Route::get('/create', [UserController::class,'create'])->name('users.create');
+
+Route::resource('curso', CursoController::class);
